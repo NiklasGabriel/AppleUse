0c2678ac4331290515f69813cf0363f9e35989a0
Niklas Gabriel <nikgab@gmx.de>
2023-01-09 16:27:59 +0100

    Home update

--- content.php
+++ content.php
@@ -7,48 +7,50 @@
 <body>
     <?php include("extra/menu.php");?>
     <div class="content">
-        <h1>Inhalt</h1>
-        <ol>
-            <li><a href="article.php?path=die_basics.php">Die Basics</a></li>
+        <div class="article">
+            <h1>Inhalt</h1>
             <ol>
-                <li><a href="article.php?path=die_basics/erster_start.php">Erster Start</a></li>
-                <li><a href="article.php?path=die_basics/die_macos_oberfläche.php">Die macOS Oberfläche</a></li>
-                <li><a href="article.php?path=die_basics/die_ios_u_ipados_oberfläche.php">Die iOS & iPadOS Oberfläche</a></li>
-                <li><a href="article.php?path=die_basics/apps.php">Apps</a></li>
+                <li><a href="article.php?path=die_basics.php">Die Basics</a></li>
+                <ol>
+                    <li><a href="article.php?path=die_basics/erster_start.php">Erster Start</a></li>
+                    <li><a href="article.php?path=die_basics/die_macos_oberfläche.php">Die macOS Oberfläche</a></li>
+                    <li><a href="article.php?path=die_basics/die_ios_u_ipados_oberfläche.php">Die iOS & iPadOS Oberfläche</a></li>
+                    <li><a href="article.php?path=die_basics/apps.php">Apps</a></li>
+                </ol>
+                <li><a href="article.php?path=die_standart_apps.php">Die Standart Apps</a></li>
+                <ol>
+                    <li><a href="article.php?path=die_standart_apps/finder.php">Finder</a></li>
+                    <li><a href="article.php?path=die_standart_apps/safari.php">Safari</a></li>
+                    <li><a href="article.php?path=die_standart_apps/systemeinstellungen.php">Systemeinstellungen</a></li>
+                    <li><a href="article.php?path=die_standart_apps/mail.php">Mail</a></li>
+                    <li><a href="article.php?path=die_standart_apps/nachrichten.php">Nachrichten</a></li>
+                    <li><a href="article.php?path=die_standart_apps/notitzen.php">Notitzen</a></li>
+                    <li><a href="article.php?path=die_standart_apps/kalender.php">Kalender</a></li>
+                    <li><a href="article.php?path=die_standart_apps/kontakte.php">Kontakte</a></li>
+                    <li><a href="article.php?path=die_standart_apps/erinnerungen.php">Erinnerungen</a></li>
+                    <li><a href="article.php?path=die_standart_apps/musik.php">Musik</a></li>
+                </ol>
+                <li><a href="article.php?path=icloud.php">iCloud</a></li>
+                <ol>
+                    <li><a href="article.php?path=icloud/appleid.php">AppleID</a></li>
+                </ol>
+                <li><a href="article.php?path=tipps_und_triks.php">Tipps und Triks</a></li>
+                <ol>
+                    <li><a href="article.php?path=tipps_und_triks/spotlight-suche.php">Spotlight-Suche</a></li>
+                    <li><a href="article.php?path=tipps_und_triks/empfelenswerte_einstellungen.php">Empfelenswerte Einstellungen</a></li>
+                    <li><a href="article.php?path=tipps_und_triks/praktische_kurzbefehle.php">Praktische Kurzbefehle</a></li>
+                </ol>
+                <li><a href="article.php?path=praktische_apps.php">Praktische Apps</a></li>
+                <ol>
+                    <li><a href="article.php?path=praktische_apps/whatsapp_web.php">WhatsApp Web</a></li>
+                    <li><a href="article.php?path=praktische_apps/signal_for_mac.php">Signal for Mac</a></li>
+                </ol>
+                <li><a href="article.php?path=ohne_zugehörigkeit.php">Ohne Zugehörigkeit</a></li>
+                <ul>
+                    <li><a href="article.php?path=ohne_zugehörigkeit/papierkorb.php">Papierkorb</a></li>
+                </ul>
             </ol>
-            <li><a href="article.php?path=die_standart_apps.php">Die Standart Apps</a></li>
-            <ol>
-                <li><a href="article.php?path=die_standart_apps/finder.php">Finder</a></li>
-                <li><a href="article.php?path=die_standart_apps/safari.php">Safari</a></li>
-                <li><a href="article.php?path=die_standart_apps/systemeinstellungen.php">Systemeinstellungen</a></li>
-                <li><a href="article.php?path=die_standart_apps/mail.php">Mail</a></li>
-                <li><a href="article.php?path=die_standart_apps/nachrichten.php">Nachrichten</a></li>
-                <li><a href="article.php?path=die_standart_apps/notitzen.php">Notitzen</a></li>
-                <li><a href="article.php?path=die_standart_apps/kalender.php">Kalender</a></li>
-                <li><a href="article.php?path=die_standart_apps/kontakte.php">Kontakte</a></li>
-                <li><a href="article.php?path=die_standart_apps/erinnerungen.php">Erinnerungen</a></li>
-                <li><a href="article.php?path=die_standart_apps/musik.php">Musik</a></li>
-            </ol>
-            <li><a href="article.php?path=icloud.php">iCloud</a></li>
-            <ol>
-                <li><a href="article.php?path=icloud/appleid.php">AppleID</a></li>
-            </ol>
-            <li><a href="article.php?path=tipps_und_triks.php">Tipps und Triks</a></li>
-            <ol>
-                <li><a href="article.php?path=tipps_und_triks/spotlight-suche.php">Spotlight-Suche</a></li>
-                <li><a href="article.php?path=tipps_und_triks/empfelenswerte_einstellungen.php">Empfelenswerte Einstellungen</a></li>
-                <li><a href="article.php?path=tipps_und_triks/praktische_kurzbefehle.php">Praktische Kurzbefehle</a></li>
-            </ol>
-            <li><a href="article.php?path=praktische_apps.php">Praktische Apps</a></li>
-            <ol>
-                <li><a href="article.php?path=praktische_apps/whatsapp_web.php">WhatsApp Web</a></li>
-                <li><a href="article.php?path=praktische_apps/signal_for_mac.php">Signal for Mac</a></li>
-            </ol>
-            <li><a href="article.php?path=ohne_zugehörigkeit.php">Ohne Zugehörigkeit</a></li>
-            <ul>
-                <li><a href="article.php?path=ohne_zugehörigkeit/papierkorb.php">Papierkorb</a></li>
-            </ul>
-        </ol>
+        </div>
     </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 <body>
     <?php include("extra/menu.php");?>
     <div class="content">
-        <div class="home">
+        <div class="article">
             <img style="width: 200px;" class="home_img_right" src="img/apple.png" alt="Apple Logo">
             <h1>Home</h1>
             <p>Diese Webseite erklärt dir das Apple Universum, dabei arbeitest du nicht mit Tutorials oder Erklärvideos, diese Seite agiert als ein großes Nachschlagwerk. Von den Basics an beginnend, kannst du dich durch Erklärungen Clicken. Viele Begriffe sind auf einen erklärenden Artikel verlinkt, dort kannst du ihn nachschlagen.</p>
-            <p>AppleUse ist nicht nur eine Plattform für Anfänger oder unsichere Nutzer, AppleUse richtet sich besonders auch an Apple user die mit dem System vertraut sind. In Abschnitten wie "Tipps und Triks" werden geniale Tipps verraten, du bekommst Empfehlungen für sinvolle Einstellungen und auch praktische Apps werden dir vorgeschlagen.</p>
+            <p>AppleUse ist nicht nur eine Plattform für Anfänger oder unsichere Nutzer, AppleUse richtet sich besonders auch an Apple User die mit dem System vertraut sind.<br>
+            In Abschnitten wie "Tipps und Triks" werden geniale Tipps verraten, du bekommst Empfehlungen für sinvolle Einstellungen und auch praktische Apps werden dir vorgeschlagen.</p>
             <br><br><br>
             <section>
                 <a style="text-decoration:underline;" href="content.php">Komplette Auflistung</a>
